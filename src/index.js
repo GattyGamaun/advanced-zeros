2cab7d9a3a8f291a39e421b20a0aa9551834e02e
GattyGamaun <gattygamaun@gmail.com>
2018-03-02 19:00:00 +0300

    zeros-advanced/feature: begin of the solution-1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
 module.exports = function getZerosCount(number, base) {
-  // let number = 46;
-  // let base = 2;
-  // let ar = [];
-  if (number % base === 0){
-  ar.push(0);
-  }
-  // console.log(ar);
-  
   let a = 5, b = 5, c = 0;
   let int = Math.floor(number / 5);
   let remain = 0;
@@ -17,5 +9,6 @@ module.exports = function getZerosCount(number, base) {
     remain += Math.floor(number / c);
   }
 
-  // return Math.round(int + remain);
+  return Math.round(int + remain);
+  
 }
